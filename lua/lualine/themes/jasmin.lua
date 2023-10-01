1c01cf16ef6a640e90ef4b9b8ae27048dc4678f2
Gurvan <jurwane@protonmail.ch>
2023-10-01 18:42:10 +0200

    Added lualine colorscheme

---
+++ lua/lualine/themes/jasmin.lua
@@ -0,0 +1,26 @@
+local c = require "jasmin.palette"
+
+return {
+  normal = {
+    a = { bg = c.regular.green, fg = c.regular.black   },
+    b = { bg = c.regular.black, fg = c.regular.white   },
+    c = { bg = c.regular.black, fg = c.regular.white   },
+  },
+  insert = {
+    a = { bg = c.regular.blue, fg = c.regular.black    },
+  },
+  visual = {
+    a = { bg = c.regular.magenta, fg = c.regular.black },
+  },
+  replace = {
+    a = { bg = c.regular.cyan, fg = c.regular.black    },
+  },
+  command = {
+    a = { bg = c.regular.yellow, fg = c.regular.black  },
+  },
+  inactive = {
+    a = { bg = c.regular.black, fg = c.regular.white   },
+  },
+}
+
+
